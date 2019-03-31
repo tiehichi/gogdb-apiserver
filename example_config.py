d7930791787ed78d4fcf2a2be43d5b94e1f3fed7
Dai Bingzhi <daibingzhi9519@outlook.com>
2019-03-31 21:54:28 +0800

    add example config

---
+++ example_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+config = dict(
+        DEBUG = True,
+        PONY = {
+            'provider': 'postgres',
+            'host': '127.0.0.1',
+            'user': 'gogdb',
+            'password': 'gogdb',
+            'database': 'gogdb'
+        }
+)
